9387f69041db3ee7edc2916c405d52bf35eb4e02
Scott Gorsky <sgorsky@gmail.com>
2021-07-14 20:01:27 -0500

    Fixed ejs issue

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
+//this throws an error
+//require('dotenv').config({ path: './config/.env'  })
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -9,27 +10,27 @@ const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
 
 //Set view engine as ejs
-//app.set('view-engine', 'ejs')
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true })
-    const db = mongoose.connection
-    db.on('error', error => console.error(error))
-    db.once('open', () => console.log('Connected ot Mongoose'))
-
-
-
-
+app.set('view engine', 'ejs')
 //Set up server views
 app.set('views', __dirname + '/views')
-app.set('layout', 'layout/layout')
+app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 
 //Set up public views folder
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true })
+    const db = mongoose.connection
+    db.on('error', error => console.error(error))
+    db.once('open', () => console.log('Connected to Mongoose'))
+
+
+
+
+
 
 app.use('/', indexRouter)
 
