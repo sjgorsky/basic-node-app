e0a8b457535fa312475bab747f88e7b701017042
Scott Gorsky <sgorsky@gmail.com>
2021-08-19 21:42:09 -0500

    dotenv fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// }
 const dotenv =  require('dotenv')
 const express = require('express')
 const app = express()
